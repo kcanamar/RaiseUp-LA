4671f565e408fd464e6965f3ebd63155c237c66b
kcanamar <99228411+kcanamar@users.noreply.github.com>
2024-01-09 20:52:31 -0800

    feature: testimonials

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import netlify from "@astrojs/netlify";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://verdant-valkyrie-138203.netlify.app/",
   output: "hybrid",
-  adapter: netlify()
+  adapter: netlify(),
+  integrations: [react()]
 });
---
+++ src/types.d.ts
@@ -0,0 +1,5 @@
+type Testimonial = {
+    name: string;
+    image?: string;
+    text: string;
+}

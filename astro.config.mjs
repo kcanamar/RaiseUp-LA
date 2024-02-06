9e1a9e7976e0662cefa1540df89a4a373ca04b08
kcanamar <99228411+kcanamar@users.noreply.github.com>
2024-02-05 16:50:47 -0800

    wip: google form iframe

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,23 @@ import netlify from "@astrojs/netlify";
 import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
+import { whyframe } from '@whyframe/core';
+import { whyframeAstro } from '@whyframe/astro';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://verdant-valkyrie-138203.netlify.app/",
   output: "hybrid",
   adapter: netlify(),
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  vite: {
+    plugins: [
+      whyframe({
+        defaultSrc: '/frames/default'
+      }),
+      whyframeAstro({
+        defaultFramework: 'react'
+      })
+    ]
+  }
 });
